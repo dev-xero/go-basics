d1da5d1e9e9b166ee73cbc13e10bdfec172fde12
Xero kun <elo48858@gmail.com>
2023-06-03 21:11:52 +0100

    `FEAT` Used immutable data types

--- hello.go
+++ hello.go
@@ -5,8 +5,10 @@ import "rsc.io/quote"
 
 var x int = 10
 var y int = 90
+const pi float64 = 3.14
 
 func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(x + y)
+	fmt.Printf("The value of PI is: %v\n", pi)
 }
