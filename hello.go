831b76ac52c367f39df387ae7f230ce0928472e6
Xero kun <elo48858@gmail.com>
2023-06-03 21:39:16 +0100

    `FEAT` Utilized boolean data types

--- hello.go
+++ hello.go
@@ -6,9 +6,14 @@ import "rsc.io/quote"
 var x int = 10
 var y int = 90
 const pi float64 = 3.14
+var isGolang bool = true
 
 func main() {
 	fmt.Println(quote.Go())
 	fmt.Println(x + y)
 	fmt.Printf("The value of PI is: %v\n", pi)
+
+	if (isGolang) {
+		fmt.Println("Currently writing in Golang")
+	}
 }
